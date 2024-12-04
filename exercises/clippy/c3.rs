857f07e56c624b22003c775a0356a0b3478d82f5
AlanL <zanjune@163.com>
2024-12-04 22:44:03 +0800

    day 3 update

--- exercises/clippy/c3.rs
+++ exercises/clippy/c3.rs
@@ -4,14 +4,12 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
+// I AM NOT DONE
+
 #[allow(unused_variables, unused_assignments)]
+
 fn main() {
     let my_option: Option<()> = None;
-    match my_option{
-        None => println!("是None!"),
-        Some(_) => println!("是Some!")
-    }
-
 
     let my_arr = &[
         -1, -2, -3,
@@ -20,7 +18,7 @@ fn main() {
     println!("My array! Here it is: {:?}", my_arr);
 
     vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", ());
+    println!("This Vec is empty, see? {:?}",());
 
     let mut value_a = 45;
     let mut value_b = 66;
--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +14,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -24,7 +23,19 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // let mut re:Vec<String> = vec![];
+    // for i in words.iter(){
+    //     let mut t = i.chars();
+    //     let mut ans = match t.next() {
+    //         None => String::new(),
+    //         Some(first) => first.to_uppercase().collect::<String>() + t.as_str(),
+    //     };
+    //     re.push(ans);
+    // }
+    // re
+
+    //planb:简洁版
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -32,7 +43,20 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // let mut re:Vec<String> = vec![];
+    // for i in words.iter(){
+    //     let mut t = i.chars();
+    //     let mut ans = match t.next() {
+    //         None => String::new(),
+    //         Some(first) => first.to_uppercase().collect::<String>() + t.as_str(),
+    //     };
+    //     re.push(ans);
+    // }
+    // re.into_iter().collect()
+
+    //planb:简洁版
+
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 #[cfg(test)]
